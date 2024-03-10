5616f806f8eb512edb3af773de1003d144a85c91
Code Help Tutor <1823890830@qq.com>
2024-03-10 04:16:16 +0000

    WeChat cstutorcs CPS506

---
+++ cps506 project haskell war /Main.hs
@@ -0,0 +1,9 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+module Main where
+
+import qualified War (main)
+
+main :: IO ()
+main = War.main
---
+++ cps506 project haskell war /War.hs
@@ -0,0 +1,24 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+module War (main, deal) where
+
+main :: IO ()
+main = do
+    putStrLn "Hello, world!"
+
+{--
+Function stub(s) with type signatures for you to fill in are given below. 
+Feel free to add as many additional helper functions as you want. 
+
+The tests for these functions can be found in src/TestSuite.hs. 
+You are encouraged to add your own tests in addition to those provided.
+
+Run the tester by executing 'cabal test' from the war directory 
+(the one containing war.cabal)
+--}
+    
+deal :: [Int] -> [Int]
+deal shuf = shuf
+               
+    
